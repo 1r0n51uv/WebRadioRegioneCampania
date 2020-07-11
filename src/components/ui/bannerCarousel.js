80979bafb58f6517e20b4d3c7ce91ba579f53ba1
Severino <s.ammirati95@gmail.com>
2020-07-11 13:26:03 +0200

    - Added image banner

--- src/components/ui/bannerCarousel.js
+++ src/components/ui/bannerCarousel.js
@@ -4,6 +4,7 @@ import '@brainhubeu/react-carousel/lib/style.css';
 import img1 from './../../assets/banner/banner1.png'
 import img2 from './../../assets/banner/banner2.png'
 import img3 from './../../assets/banner/banner3.jpg'
+import img4 from './../../assets/banner/banner4.jpg'
 class BannerCarousel extends Component {
     render() {
         return (
@@ -15,6 +16,7 @@ class BannerCarousel extends Component {
                     <img onClick={() => window.open('https://www.facebook.com/Studio-Odontoiatrico-Castaldo-1505004786417059') } className="img-fluid" src={img1} />
                     <img onClick={() => window.open('https://www.facebook.com/Streetdance-FSV-394427487946405')} className="img-fluid" src={img2} />
                     <img onClick={() => window.open('https://www.ldgservice.it')} className="img-fluid" src={img3} />
+                    <img onClick={() => window.open('https://www.facebook.com/daNanduccio')} className="img-fluid" src={img4} />
 
                 </Carousel>
 
