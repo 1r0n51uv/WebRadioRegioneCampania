d59bbde931a490be3a90e9f475d9dd4d219abb4a
1r0n51uv <s.ammirati95@gmail.com>
2021-02-13 11:16:59 +0100

    Fixed facebook panel width

--- src/components/fbPage.js
+++ src/components/fbPage.js
@@ -6,7 +6,7 @@ class FbPage extends Component {
             <div className="text-center">
                 <iframe
                     src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FWeb-Radio-Regione-Campania-103449527993974%2F%3Fepa%3DSEARCH_BOX&tabs=timeline&width=450&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=194721887735996"
-                    width="450" height="450" style={{border: 'none', overflow: 'hidden'}} scrolling="no" frameBorder="0"
+                    width="350" height="450" style={{border: 'none', overflow: 'hidden'}} scrolling="no" frameBorder="0"
                     allowTransparency="true" allow="encrypted-media"/>
             </div>
         );
